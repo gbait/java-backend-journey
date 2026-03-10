1810fd07b336c547f522cacdf9086102367e3862
gbait <gonzalo.barbosat@gmail.com>
2026-03-10 16:32:23 +0100

    Add Day04ConditionsMonitoring class with main method

---
+++ Day04ConditionsMonitoring.java
@@ -0,0 +1,10 @@
+public class Day04ConditionsMonitoring {
+    public static void main(String[] args) {
+        int[] consumos = {4, 9, 12, 17, 0};
+        System.out.println( "Rack " + (i + 1) + " -> " + consumo + "kW");
+
+    if (consumo == 0) {
+        System.out.println("Estado: FALLO o apagado");
+
+    } else if (consumo <= 10) {
+        System.out.println()
